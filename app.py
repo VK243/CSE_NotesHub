f1ab7ac5ae935120fc3c049188e98eea00cfa129
Varshith Kumar <76109914+VK243@users.noreply.github.com>
2022-01-11 14:52:41 +0530

    adding new files and connecting to local db

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask, render_template
-app = Flask (__name__)
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import json
+from datetime import datetime
+
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
+
+local_server = True
+app = Flask(__name__)
+if(local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    phone_num = db.Column(db.String(13), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', params=params)
 
 @app.route("/login")
 def login():
@@ -15,11 +37,40 @@ def signin():
 
 @app.route("/search")
 def search():
-    return render_template('search.html')
+    return render_template('search.html', params=params)
 
 @app.route("/admin")
-def admin():
-    return render_template('admin.html')
+def adminlogin():
+    return render_template('adminlogin.html')
 
-app.run()
+@app.route("/upload")
+def upload():
+    return render_template('upload.html', params=params)
+
+@app.route("/subject")
+def subject():
+    return render_template('subject.html', params=params)
+
+@app.route("/document")
+def document():
+    return render_template('document.html', params=params)
+
+@app.route("/contact", methods = ['GET', 'POST'])
+def contact():
+    if(request.method=='POST'):
+        '''add entry to database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contact(name = name, email = email, phone_num = phone, msg = message, date=datetime.now())
+        db.session.add(entry)
+        db.session.commit()
+    return render_template('contact.html', params=params)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', params=params)
+
+app.run(debug=True)
 
